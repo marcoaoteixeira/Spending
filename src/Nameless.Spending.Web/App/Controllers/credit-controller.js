e12f670348d3da0e5cfdc8da1317c2e985282a6f
marcoaoteixeira <marcoaoteixeira@gmail.com>
2015-06-22 07:03:35 -0300

    Added gulpfile and js

---
+++ src/Nameless.Spending.Web/App/Controllers/credit-controller.js
@@ -0,0 +1,18 @@
+/*global angular*/
+
+(function () {
+	'use strict';
+
+	/* Define controllers ************************************************** */
+	angular
+		.module('App')
+		.controller('CreditController', CreditController);
+	/* ************************************************** Define controllers */
+
+	/* Credit Controller *************************************************** */
+	CreditController.$inject = ['$scope'];
+
+	function CreditController($scope) {
+	};
+	/* *************************************************** Credit Controller */
+}());
---
+++ src/Nameless.Spending.Web/App/application.js
@@ -0,0 +1,19 @@
+/*global angular*/
+
+(function () {
+    'use strict';
+
+    angular
+		.module('App', [
+			'ngAnimate',
+			'ngAria',
+			'ngCookies',
+			'ngMessages',
+			'ngResource',
+			'ngRoute',
+			'ngSanitize',
+			'ngTouch',
+			'angular-loading-bar',
+			'LocalStorageModule'
+		]);
+}());
